3c0df0da6ef03af80354a68963344e8a0532d8dc
zulymargs <zulymar.garcia@upr.edu>
2023-12-06 03:10:43 -0400

    start

--- crud/admin/index.php
+++ crud/admin/index.php
@@ -1,16 +1,123 @@
 <?php
-session_start(); // Start the session
+session_start();
 $titulo = "Pre-Matrícula UPRA";
 
+// Include the database connection code
+include_once("../db_info.php");
+
+if ($dbc->connect_error) {
+    die("Connection failed: " . $dbc->connect_error);
+}
 // Check if the user is logged in
 if (isset($_SESSION['admID'])) {
     echo "<h2>Bienvenido, {$_SESSION['admin_lastnameP']} {$_SESSION['admin_lastnameM']}, {$_SESSION['admin_name']}| <a href='../logout.php'>Logout</a></h2>";
 } else {
-    echo "<p>Session not active <a href='login.php'>Login</a></p>";
+    echo "<p>Session not active <a href='../logout.php'>Login</a></p>";
 }
 
-// Now you can use $_SESSION['student_id'] to get the logged-in student's ID
-$student_id = $_SESSION['admID'];
+$studentID = $_SESSION['admID'];
+
+// Function to validate a course ID
+function isValidCourseID($courseID) {
+    return preg_match('/^[A-Z]{4}\d{4}$/', $courseID) === 1;
+}
+
+// Function to validate a section ID
+function isValidSectionID($sectionID) {
+    return strlen($sectionID) === 3;
+}
+
+// Function to validate a natural number
+function isValidNaturalNumber($number) {
+    return ctype_digit($number) && $number > 0;
+}
+
+// Function to create a new course with validation
+function createCourse($courseName, $courseID) {
+    global $dbc;
+
+    if (!isValidCourseID($courseID)) {
+        return false; // Invalid course ID
+    }
+
+    $sql = "INSERT INTO courses (course_name, course_id) VALUES ('$courseName', '$courseID')";
+    return $dbc->query($sql);
+}
+
+// Function to create a new section with validation
+function createSection($sectionName, $courseID, $capacity, $credits) {
+    global $dbc;
+
+    if (!isValidCourseID($courseID)) {
+        return false; // Invalid course ID
+    }
+
+    if (!isValidSectionID($sectionName)) {
+        return false; // Invalid section ID
+    }
+
+    if (!isValidNaturalNumber($capacity)) {
+        return false; // Invalid capacity
+    }
+
+    if (!isValidNaturalNumber($credits)) {
+        return false; // Invalid credits
+    }
+
+    // Check if the course exists
+    $courseExists = $dbc->query("SELECT 1 FROM courses WHERE course_id = '$courseID'")->fetch_row();
+
+    if (!$courseExists) {
+        return false; // Course does not exist
+    }
+
+    $sql = "INSERT INTO sections (section_name, course_id, capacity, credits) VALUES ('$sectionName', '$courseID', '$capacity', '$credits')";
+    return $dbc->query($sql);
+}
+
+// Handle form submissions
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["createCourse"])) {
+        $courseName = $_POST["courseName"];
+        $courseID = $_POST["courseID"];
+
+        if (createCourse($courseName, $courseID)) {
+            echo "Course created successfully!";
+        } else {
+            echo "Error creating course. Please check your input.";
+        }
+    } elseif (isset($_POST["createSection"])) {
+        $sectionName = $_POST["sectionName"];
+        $courseID = $_POST["courseID"];
+        $capacity = $_POST["capacity"];
+        $credits = $_POST["credits"];
+
+        if (createSection($sectionName, $courseID, $capacity, $credits)) {
+            echo "Section created successfully!";
+        } else {
+            echo "Error creating section. Please check your input.";
+        }
+    } elseif (isset($_POST["enrollStudent"])) {
+        $sectionID = $_POST["sectionID"];
+
+        // Add your enrollment logic here
+        enrollStudent($studentID, $sectionID);
+    }
+}
+
+// Retrieve search results
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["search_query"])) {
+    $search_query = $_POST["search_query"];
+    $query = "SELECT * FROM courses NATURAL JOIN sections
+              WHERE course_id LIKE '%$search_query%'
+              ORDER BY course_id ASC, section_id ASC";
+
+    $result = $dbc->query($query);
+
+    if (!$result) {
+        die("Error executing the query: " . $dbc->error);
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -29,7 +136,7 @@ $student_id = $_SESSION['admID'];
         <form action="index.php" method="post">
             <!-- ... existing form fields ... -->
             <tr>
-                <td width="140" align="right">Buscar curso:</td>
+                <td width="140" align="right">Buscar curso para:</td>
                 <td><input type="text" name="search_query" size="50" maxlength="255" /></td>
             </tr>
             <!-- ... existing form fields ... -->
@@ -40,34 +147,7 @@ $student_id = $_SESSION['admID'];
         </form>
 
         <?php
-        /* código */
-        include_once("../db_info.php");
-
-        // Check if the connection was successful
-        if ($dbc->connect_error) {
-            die("Connection failed: " . $dbc->connect_error);
-        }
-
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            // ... existing code ...
-
-            if (isset($_POST['search_query'])) {
-                $search_query = $_POST['search_query'];
-                // Modify your SQL query to include the search condition
-                $query = "SELECT * FROM course NATURAL JOIN section
-                          WHERE course_id LIKE '%$search_query%'
-                          ORDER BY course_id ASC, section_id ASC";
-
-                // ... rest of the code ...
-                $result = $dbc->query($query);
-                if ($result) {
-                    // ... existing code ...
-                } else {
-                    die("Error executing the query: " . $dbc->error);
-                }
-            }
-        }
-
+        // Display the search results
         if ($result->num_rows > 0) {
             echo "<h3>Search Results:</h3>";
             echo "<table border='1'>";
@@ -77,12 +157,17 @@ $student_id = $_SESSION['admID'];
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>";
                 echo "<td>" . $row['course_id'] . "-" . $row['section_id'] . "</td>";
-                echo "<td>" . $row['title'] . "</td>";
+                echo "<td>" . $row['course_name'] . "</td>";
                 echo "<td>" . $row['capacity'] . "</td>";
                 $courseSectionId = $row['course_id'] . "-" . $row['section_id'];
+
                 // Display the "Enroll" button only if the student is not already enrolled
                 if (!$isEnrolled) {
-                    echo "<td><button class='enroll-button' onclick='enrollCourse(\"$courseSectionId\")'>OK</button></td>";
+                    echo "<td><form method='post' action='index.php'>
+                            <input type='hidden' name='enrollStudent' value='enroll'>
+                            <input type='hidden' name='sectionID' value='$courseSectionId'>
+                            <input type='submit' class='enroll-button' value='OK'>
+                          </form></td>";
                 } else {
                     echo "<td>Sección Llena</td>";
                 }
@@ -95,49 +180,6 @@ $student_id = $_SESSION['admID'];
         } else {
             echo "<p>No results found.</p>";
         }
-        // Process enrollment when the button is clicked
-        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['enroll_course'])) {
-            $courseSectionId = $_POST['enroll_course'];
-            list($courseId, $sectionId) = explode("-", $courseSectionId);
-
-            // Step 1: Update Enrollment Status
-            $updateQuery = "UPDATE enrollment SET status = 1 WHERE course_id = '$courseId' AND section_id = '$sectionId'";
-            $dbc->query($updateQuery);
-
-            // Step 2: Priority Logic
-            $priorityQuery = "SELECT e.student_id, s.year_of_study, e.timestamp
-                            FROM enrollment e
-                            JOIN student s ON e.student_id = s.student_id
-                            WHERE e.course_id = '$courseId' AND e.section_id = '$sectionId'
-                            ORDER BY s.year_of_study DESC, e.timestamp ASC";
-            $priorityResult = $dbc->query($priorityQuery);
-
-            // Step 3: Capacity Constraint
-            $sectionQuery = "SELECT capacity FROM section WHERE course_id = '$courseId' AND section_id = '$sectionId'";
-            $sectionResult = $dbc->query($sectionQuery);
-            $sectionRow = $sectionResult->fetch_assoc();
-            $capacity = $sectionRow['capacity'];
-
-            // Update the enrollment status based on priority and capacity
-            $count = 0;
-            while ($row = $priorityResult->fetch_assoc()) {
-                $studentId = $row['student_id'];
-                $updateQuery = "UPDATE enrollment SET status = 1 WHERE course_id = '$courseId' AND section_id = '$sectionId' AND student_id = '$studentId'";
-                $dbc->query($updateQuery);
-                $count++;
-
-                // Check if the maximum capacity is reached
-                if ($count >= $capacity) {
-                    break;
-                }
-            }
-
-            // Redirect or display a success message
-            header("Location: index.php");
-            exit();
-        }
-
-
         ?>
     </div>
 </body>
