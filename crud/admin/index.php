0f0648295fc08803cdda06418ae32674892883d4
eddfig <eddy.figueroa1@upr.edu>
2023-12-06 00:33:28 -0400

    admin function

--- crud/admin/index.php
+++ crud/admin/index.php
@@ -71,7 +71,7 @@ $student_id = $_SESSION['admID'];
         if ($result->num_rows > 0) {
             echo "<h3>Search Results:</h3>";
             echo "<table border='1'>";
-            echo "<tr><th>Course ID</th><th>Title</th><th>Availability</th></tr>";
+            echo "<tr><th>Course ID</th><th>Title</th><th>Availability</th><th>Enroll Waitinglist</th></tr>";
 
             // Loop through the results and display them in a table
             while ($row = $result->fetch_assoc()) {
@@ -79,12 +79,13 @@ $student_id = $_SESSION['admID'];
                 echo "<td>" . $row['course_id'] . "-" . $row['section_id'] . "</td>";
                 echo "<td>" . $row['title'] . "</td>";
                 echo "<td>" . $row['capacity'] . "</td>";
+                $courseSectionId = $row['course_id'] . "-" . $row['section_id'];
                 // Display the "Enroll" button only if the student is not already enrolled
-                // if (!$isEnrolled) {
-                //     echo "<td><button class='enroll-button' onclick='enrollCourse(\"$courseSectionId\")'>Enroll</button></td>";
-                // } else {
-                //     echo "<td>Already Enrolled</td>";
-                // }
+                if (!$isEnrolled) {
+                    echo "<td><button class='enroll-button' onclick='enrollCourse(\"$courseSectionId\")'>OK</button></td>";
+                } else {
+                    echo "<td>Sección Llena</td>";
+                }
 
                 echo "</tr>";
                 // ... display other relevant information ...
@@ -94,6 +95,49 @@ $student_id = $_SESSION['admID'];
         } else {
             echo "<p>No results found.</p>";
         }
+        // Process enrollment when the button is clicked
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['enroll_course'])) {
+            $courseSectionId = $_POST['enroll_course'];
+            list($courseId, $sectionId) = explode("-", $courseSectionId);
+
+            // Step 1: Update Enrollment Status
+            $updateQuery = "UPDATE enrollment SET status = 1 WHERE course_id = '$courseId' AND section_id = '$sectionId'";
+            $dbc->query($updateQuery);
+
+            // Step 2: Priority Logic
+            $priorityQuery = "SELECT e.student_id, s.year_of_study, e.timestamp
+                            FROM enrollment e
+                            JOIN student s ON e.student_id = s.student_id
+                            WHERE e.course_id = '$courseId' AND e.section_id = '$sectionId'
+                            ORDER BY s.year_of_study DESC, e.timestamp ASC";
+            $priorityResult = $dbc->query($priorityQuery);
+
+            // Step 3: Capacity Constraint
+            $sectionQuery = "SELECT capacity FROM section WHERE course_id = '$courseId' AND section_id = '$sectionId'";
+            $sectionResult = $dbc->query($sectionQuery);
+            $sectionRow = $sectionResult->fetch_assoc();
+            $capacity = $sectionRow['capacity'];
+
+            // Update the enrollment status based on priority and capacity
+            $count = 0;
+            while ($row = $priorityResult->fetch_assoc()) {
+                $studentId = $row['student_id'];
+                $updateQuery = "UPDATE enrollment SET status = 1 WHERE course_id = '$courseId' AND section_id = '$sectionId' AND student_id = '$studentId'";
+                $dbc->query($updateQuery);
+                $count++;
+
+                // Check if the maximum capacity is reached
+                if ($count >= $capacity) {
+                    break;
+                }
+            }
+
+            // Redirect or display a success message
+            header("Location: index.php");
+            exit();
+        }
+
+
         ?>
     </div>
 </body>
