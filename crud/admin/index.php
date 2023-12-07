28df6a4ac3df2d6ce68e920e17c9f3ff20886df7
zulymargs <zulymar.garcia@upr.edu>
2023-12-07 09:47:08 -0400

    TRY

--- crud/admin/index.php
+++ crud/admin/index.php
@@ -2,80 +2,97 @@
 session_start();
 $titulo = "Pre-Matrícula UPRA";
 
-// Include the database connection code
+
 include_once("../db_info.php");
 
 if ($dbc->connect_error) {
     die("Connection failed: " . $dbc->connect_error);
 }
-// Check if the user is logged in
+
+
 if (isset($_SESSION['admID'])) {
     echo "<h2>Bienvenido, {$_SESSION['admin_lastnameP']} {$_SESSION['admin_lastnameM']}, {$_SESSION['admin_name']}| <a href='../logout.php'>Logout</a></h2>";
 } else {
     echo "<p>Session not active <a href='../logout.php'>Login</a></p>";
 }
 
-$studentID = $_SESSION['admID'];
+$adminID = $_SESSION['admID'];
 
-// Function to validate a course ID
+
 function isValidCourseID($courseID) {
     return preg_match('/^[A-Z]{4}\d{4}$/', $courseID) === 1;
 }
 
-// Function to validate a section ID
+
 function isValidSectionID($sectionID) {
     return strlen($sectionID) === 3;
 }
 
-// Function to validate a natural number
+
 function isValidNaturalNumber($number) {
     return ctype_digit($number) && $number > 0;
 }
 
-// Function to create a new course with validation
+
 function createCourse($courseName, $courseID) {
     global $dbc;
 
     if (!isValidCourseID($courseID)) {
-        return false; // Invalid course ID
+        return false; 
     }
 
-    $sql = "INSERT INTO courses (course_name, course_id) VALUES ('$courseName', '$courseID')";
+    $sql = "INSERT INTO courses (title, course_id) VALUES ('$courseName', '$courseID')";
     return $dbc->query($sql);
 }
 
-// Function to create a new section with validation
+
 function createSection($sectionName, $courseID, $capacity, $credits) {
     global $dbc;
 
     if (!isValidCourseID($courseID)) {
-        return false; // Invalid course ID
+        return false; 
     }
 
     if (!isValidSectionID($sectionName)) {
-        return false; // Invalid section ID
+        return false; 
     }
 
     if (!isValidNaturalNumber($capacity)) {
-        return false; // Invalid capacity
+        return false; 
     }
-
     if (!isValidNaturalNumber($credits)) {
-        return false; // Invalid credits
+        return false; 
     }
 
-    // Check if the course exists
     $courseExists = $dbc->query("SELECT 1 FROM courses WHERE course_id = '$courseID'")->fetch_row();
 
     if (!$courseExists) {
-        return false; // Course does not exist
+        return false; 
     }
 
     $sql = "INSERT INTO sections (section_name, course_id, capacity, credits) VALUES ('$sectionName', '$courseID', '$capacity', '$credits')";
     return $dbc->query($sql);
 }
 
-// Handle form submissions
+function enrollStudent($studentID, $sectionID) {
+    global $dbc;
+
+    $enrollmentResult = $dbc->query("SELECT 1 FROM enrollment WHERE student_id = '$studentID' AND section_id = '$sectionID'");
+    $isEnrolled = $enrollmentResult && $enrollmentResult->num_rows > 0;
+
+    if (!$isEnrolled) {
+        $insertResult = $dbc->query("INSERT INTO enrollment (student_id, section_id, status) VALUES ('$studentID', '$sectionID', 1)");
+
+        if ($insertResult) {
+            echo "Student enrolled successfully!";
+        } else {
+            echo "Error enrolling student. Please try again.";
+        }
+    } else {
+        echo "Student is already enrolled in this section.";
+    }
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["createCourse"])) {
         $courseName = $_POST["courseName"];
@@ -99,13 +116,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     } elseif (isset($_POST["enrollStudent"])) {
         $sectionID = $_POST["sectionID"];
+        $enrollmentID = $_POST["enrollmentID"];
 
-        // Add your enrollment logic here
-        enrollStudent($studentID, $sectionID);
+        $enrollmentResult = $dbc->query("SELECT student_id FROM enrollment WHERE enrollment_id = '$enrollmentID'");
+        if ($enrollmentResult && $enrollmentResult->num_rows > 0) {
+            $enrollmentData = $enrollmentResult->fetch_assoc();
+            $studentID = $enrollmentData['student_id'];
+
+            enrollStudent($studentID, $sectionID);
+        } else {
+            echo "Error retrieving enrollment information.";
+        }
     }
 }
 
-// Retrieve search results
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["search_query"])) {
     $search_query = $_POST["search_query"];
     $query = "SELECT * FROM courses NATURAL JOIN sections
@@ -132,14 +156,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["search_query"])) {
     <div>
         <h1>Pre-Matrícula UPRA</h1>
 
-        <!-- Existing form fields -->
         <form action="index.php" method="post">
-            <!-- ... existing form fields ... -->
             <tr>
                 <td width="140" align="right">Buscar curso para:</td>
                 <td><input type="text" name="search_query" size="50" maxlength="255" /></td>
             </tr>
-            <!-- ... existing form fields ... -->
             <tr>
                 <td></td>
                 <td><input type="submit" class="formbutton" name="submit" value="Buscar Curso" /></td>
@@ -147,39 +168,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["search_query"])) {
         </form>
 
         <?php
-        // Display the search results
-        if ($result->num_rows > 0) {
-            echo "<h3>Search Results:</h3>";
-            echo "<table border='1'>";
-            echo "<tr><th>Course ID</th><th>Title</th><th>Availability</th><th>Enroll Waitinglist</th></tr>";
 
-            // Loop through the results and display them in a table
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $row['course_id'] . "-" . $row['section_id'] . "</td>";
-                echo "<td>" . $row['course_name'] . "</td>";
-                echo "<td>" . $row['capacity'] . "</td>";
-                $courseSectionId = $row['course_id'] . "-" . $row['section_id'];
+    if (isset($result)) {
+            if ($result === null) {
+                echo "<p>Error executing the query: " . $dbc->error . "</p>";
+            } elseif ($result->num_rows > 0) {
+                echo "<h3>Search Results:</h3>";
+                echo "<table border='1'>";
+                echo "<tr><th>Course ID</th><th>Title</th><th>Availability</th><th>Enroll Waitinglist</th></tr>";
+        
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row['course_id'] . "-" . $row['section_id'] . "</td>";
+                    echo "<td>" . $row['title'] . "</td>";
+                    echo "<td>" . $row['capacity'] . "</td>";
+                    $courseName = isset($row['title']) ? $row['title'] : 'N/A';
+    echo "<td>" . $courseName . "</td>";
 
-                // Display the "Enroll" button only if the student is not already enrolled
-                if (!$isEnrolled) {
-                    echo "<td><form method='post' action='index.php'>
-                            <input type='hidden' name='enrollStudent' value='enroll'>
-                            <input type='hidden' name='sectionID' value='$courseSectionId'>
-                            <input type='submit' class='enroll-button' value='OK'>
-                          </form></td>";
-                } else {
-                    echo "<td>Sección Llena</td>";
+    echo "<td>" . $row['capacity'] . "</td>";
+    $courseSectionId = $row['course_id'] . "-" . $row['section_id'];
+
+
+                    if (isset($isEnrolled) && !$isEnrolled) {
+                        echo "<td><form method='post' action='index.php'>
+                                <input type='hidden' name='enrollStudent' value='enroll'>
+                                <input type='hidden' name='sectionID' value='$courseSectionId'>
+                                <input type='hidden' name='enrollmentID' value='{$row['enrollment_id']}'>
+                                <input type='submit' class='enroll-button' value='OK'>
+                              </form></td>";
+                    } else {
+                        echo "<td>Sección Llena</td>";
+                    }
+                
+                    echo "</tr>"; 
                 }
-
-                echo "</tr>";
-                // ... display other relevant information ...
-            }
 
             echo "</table>";
         } else {
             echo "<p>No results found.</p>";
         }
+    } else {
+        echo "<p>No results found.</p>";
+    }
         ?>
     </div>
 </body>
